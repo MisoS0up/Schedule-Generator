59b406510ce56622a614ee56f08e603d66e7cf9c
Dan Joshua Naing <joshuanaing212@gmail.com>
2023-06-05 19:12:17 +0800

    DarkMode option added Redesign-check

--- Sched/wwwroot/js/site.js
+++ Sched/wwwroot/js/site.js
@@ -11,3 +11,28 @@ $(document).ready(function () {
         $('#errorToast').toast('hide');
     }, 5000);
 });
+
+
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    // Get the current mode preference from local storage
+    var darkModeEnabled = localStorage.getItem('darkModeEnabled');
+
+    // Toggle the mode preference
+    darkModeEnabled = darkModeEnabled === 'true' ? 'false' : 'true';
+
+    // Update the class of the body element
+    document.body.classList.toggle('dark-mode');
+
+    // Save the updated mode preference to local storage
+    localStorage.setItem('darkModeEnabled', darkModeEnabled);
+}
+
+// Check if dark mode preference is set and apply it on page load
+document.addEventListener('DOMContentLoaded', function () {
+    var darkModeEnabled = localStorage.getItem('darkModeEnabled');
+    if (darkModeEnabled === 'true') {
+        document.body.classList.add('dark-mode');
+    }
+});
